89173aa37a814bfeb275e287d59ce604e481124a
argymeg <argymeg@gmail.com>
2019-07-08 17:22:04 +0100

    A way to calculate the score - hard to implement the population weighing though

--- R/calculations.R
+++ R/calculations.R
@@ -3,6 +3,37 @@ getScore <- function(pair, segmentTable){
   sample1 <- segmentTable[segmentTable$SampleID == pair[1],]
   sample2 <- segmentTable[segmentTable$SampleID == pair[2],]
 
+  sample1$nTotal <- sample1$nMajor + sample1$nMinor
+  sample2$nTotal <- sample2$nMajor + sample2$nMinor
+
+  sample1$status <- ifelse(sample1$nTotal < 2, "loss", ifelse(sample1$nTotal == 2, "norm", "gain"))
+  sample2$status <- ifelse(sample2$nTotal < 2, "loss", ifelse(sample2$nTotal == 2, "norm", "gain"))
+
+  sample1_lists <- c(list(), list(sample1["loss", on = "status"]), list(sample1["norm", on = "status"]), list(sample1["gain", on = "status"]))
+  sample2_lists <- c(list(), list(sample2["loss", on = "status"]), list(sample2["norm", on = "status"]), list(sample2["gain", on = "status"]))
+
+  # sample1_lists <- c(list(), list(sample1["loss", on = "status"]), list(sample1["gain", on = "status"]))
+  # sample2_lists <- c(list(), list(sample2["loss", on = "status"]), list(sample2["gain", on = "status"]))
+  #
+
+  merged_lists <- mapply(function(x, y){list(merge(x, y, by = c("Chr", "Start"), all = TRUE))}, sample1_lists, sample2_lists)
+  binary_lists <- lapply(merged_lists, function(x){apply(x, 1, function(y){!any(is.na(y))})})
+
+  concordant_list <- lapply(binary_lists, sum)
+  discordant_list <- lapply(binary_lists, function(x){length(x) - sum(x)})
+
+  nconcordant <- unlist(concordant_list)
+  ndiscordant <- unlist(discordant_list)
+
+  # lapply(ssadistsss_lists, function(x){apply(x, 1, function(y){!any(is.na(y))})})
+  # lapply(ssadistsss_lists, function(x){table(apply(x, 1, function(y){any(is.na(y))}))})
+  #
+  # lapply(sample1_lists, function(s1, s2){lapply(s1$Start, breakpointScore, s2$Start)}, sample2_lists)
+  # unlist(mapply(function(s1, s2){lapply(s1$Start, breakpointScore, s2$Start)}, sample1_lists, sample2_lists))
+
+
+
+
   scores <- unlist(lapply(sample1$Start, breakpointScore, sample2$Start))
   return(scores)
 }
@@ -14,3 +45,5 @@ breakpointScore <- function(breakpoint, sample2){
     return(0)
   }
 }
+
+
