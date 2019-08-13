4f98acbcc4b4403b9874f9fb2d09005c359f69eb
argymeg <argymeg@gmail.com>
2019-08-13 16:05:16 +0100

    Add VCF parsing

--- R/readFunctions.R
+++ R/readFunctions.R
@@ -5,6 +5,20 @@ readAlleleSpecific <- function(directory, pattern = "*_Segments_AbsCN_alleleSpec
   return(segmentTable)
 }
 
+readVCF <- function(directory, pattern = "*.vcf"){
+  parseVCF <- function(x){
+    vcf <- read.vcfR(x, verbose = FALSE)
+    extracted_fields <- vcfR2tidy(vcf, info_only = TRUE, info_fields = c("SVTYPE", "END", "BINS", "SVLEN"))$fix[,c("CHROM", "POS", "END", "BINS", "SVTYPE", "SVLEN")]
+    samplename <- colnames(vcf@gt)[2]
+    return(cbind(samplename, extracted_fields))
+  }
+  fileList <- dir(directory, pattern, full.names = TRUE)
+  segmentList <- lapply(fileList, parseVCF)
+  segmentList <- segmentList[!unlist(lapply(segmentList, function(x){any(is.na(x))}))]
+  segmentTable <- data.table::rbindlist(segmentList)
+  return(segmentTable)
+}
+
 inferPairs <- function(segmentTable){
   samples <- unique(segmentTable$SampleID)
   patients <- unique(sub("_.+", "", samples))
